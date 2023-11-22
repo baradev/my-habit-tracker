c4f80b67217fbb3646469ee638e993930dcdafad
kbarushkaa <barboraklusackova@gmail.com>
2023-11-22 22:00:12 +1300

    plain table, green cells

--- app/HabitTracker.tsx
+++ app/HabitTracker.tsx
@@ -1,36 +1,38 @@
 "use client";
 
+// pages/index.js
 import { useState } from "react";
 import styles from "./HabitTracker.module.css";
 
 const HabitTracker = () => {
-  const [dayStatus, setDayStatus] = useState({});
+  const [completedDays, setCompletedDays] = useState([]);
 
-  const toggleStatus = (day) => {
-    setDayStatus((prevStatus) => ({
-      ...prevStatus,
-      [day]: prevStatus[day] === "success" ? "failure" : "success",
-    }));
+  const toggleCompleted = (day) => {
+    const updatedDays = [...completedDays];
+    const index = updatedDays.indexOf(day);
+
+    if (index === -1) {
+      updatedDays.push(day);
+    } else {
+      updatedDays.splice(index, 1);
+    }
+
+    setCompletedDays(updatedDays);
   };
 
   return (
     <div className={styles.habitTracker}>
-      {[...Array(30)].map((_, index) => {
-        const day = index + 1;
-        const status = dayStatus[day];
-
-        return (
-          <div
-            key={day}
-            className={`${styles.habitDay} ${
-              status === "success" ? styles.success : styles.failure
-            }`}
-            onClick={() => toggleStatus(day)}
-          >
-            {day}
-          </div>
-        );
-      })}
+      {[...Array(30)].map((_, index) => (
+        <div
+          key={index + 1}
+          className={`${styles.habitDay} ${
+            completedDays.includes(index + 1) ? styles.completed : ""
+          }`}
+          onClick={() => toggleCompleted(index + 1)}
+        >
+          {index + 1}
+        </div>
+      ))}
     </div>
   );
 };
