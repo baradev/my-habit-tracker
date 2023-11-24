ea25f2a5fec0e8040c11f7bdc4082f98a12a47b2
kbarushkaa <barboraklusackova@gmail.com>
2023-11-24 16:42:29 +1300

    cells coresponds with number of days in the month

--- app/HabitTracker.tsx
+++ app/HabitTracker.tsx
@@ -1,18 +1,39 @@
 "use client";
-import React, { useState } from "react";
 
-const HabitTracker = () => {
-  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1); // Current month
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear()); // Current year
+import React, { useState, useEffect } from "react";
 
-  const daysInMonth = new Date(selectedYear, selectedMonth, 0).getDate();
-  const initialHabitData = Array.from({ length: daysInMonth }, () => false);
-  const [doneHabitData, setDoneHabitData] = useState(initialHabitData);
+interface HabitTrackerProps {}
 
-  const toggleDay = (dayIndex: number) => {
+const HabitTracker: React.FC<HabitTrackerProps> = () => {
+  const [selectedMonth, setSelectedMonth] = useState<number>(
+    new Date().getMonth() + 1
+  ); // Current month
+  const [selectedYear, setSelectedYear] = useState<number>(
+    new Date().getFullYear()
+  ); // Current year
+  const [habitName, setHabitName] = useState<string>("");
+  const [habits, setHabits] = useState<string[]>([]); // Array to store habits
+  const [doneHabitData, setDoneHabitData] = useState<boolean[][]>([]);
+
+  useEffect(() => {
+    const daysInMonth = new Date(selectedYear, selectedMonth, 0).getDate();
+    const initialHabitData: boolean[][] = Array.from(
+      { length: daysInMonth },
+      () => Array(habits.length).fill(false)
+    );
+    setDoneHabitData(initialHabitData);
+  }, [selectedMonth, selectedYear, habits]);
+
+  const toggleDay = (dayIndex: number, habitIndex: number) => {
     const updatedDoneHabitData = [...doneHabitData];
-    updatedDoneHabitData[dayIndex] = !doneHabitData[dayIndex];
+    updatedDoneHabitData[dayIndex][habitIndex] =
+      !doneHabitData[dayIndex][habitIndex];
     setDoneHabitData(updatedDoneHabitData);
+  };
+
+  const addHabit = () => {
+    setHabits([...habits, habitName]);
+    setHabitName("");
   };
 
   return (
@@ -49,9 +70,19 @@ const HabitTracker = () => {
         </label>
       </div>
 
+      <div>
+        <input
+          type="text"
+          value={habitName}
+          onChange={(e) => setHabitName(e.target.value)}
+          placeholder="Enter Habit Name"
+        />
+        <button onClick={addHabit}>Add</button>
+      </div>
+
       <div style={{ display: "flex", flexDirection: "column" }}>
         <div style={{ display: "flex", height: 30 }}>
-          {Array.from({ length: daysInMonth }, (_, i) => (
+          {Array.from({ length: doneHabitData.length }, (_, i) => (
             <div
               key={i + 1}
               style={{
@@ -66,21 +97,26 @@ const HabitTracker = () => {
           ))}
         </div>
 
-        <div style={{ display: "flex", height: 30 }}>
-          {doneHabitData.map((isDone, index) => (
-            <div
-              key={index + 1}
-              onClick={() => toggleDay(index)}
-              style={{
-                flex: 1,
-                cursor: "pointer",
-                backgroundColor: isDone ? "green" : "white",
-                border: "1px solid black",
-                width: 30,
-              }}
-            ></div>
-          ))}
-        </div>
+        {habits.map((habit, habitIndex) => (
+          <div key={habitIndex}>
+            <div style={{ display: "flex", height: 30 }}>
+              {doneHabitData.map((isDone, dayIndex) => (
+                <div
+                  key={dayIndex + 1}
+                  onClick={() => toggleDay(dayIndex, habitIndex)}
+                  style={{
+                    flex: 1,
+                    cursor: "pointer",
+                    backgroundColor: isDone[habitIndex] ? "green" : "white",
+                    border: "1px solid black",
+                    width: 30,
+                  }}
+                ></div>
+              ))}
+            </div>
+            <div>{habit}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
