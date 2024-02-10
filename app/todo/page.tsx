4afd7175871286af7cb8128d3a6590cc63004953
Barbora Klusackova <barboraklusackova@gmail.com>
2024-02-11 11:30:56 +1300

    fix: quotes, also did that already

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -6,7 +6,6 @@ const ToDo = () => {
       <ul>
         <li>Connect with database</li>
         <li> Initial page when the app is first use</li>
-        <li>when name not fill and press arrow - fill "New Habit"</li>
         <li>when click on homepage - loaded on current month</li>
       </ul>
     </div>
