0db66b4d613f041cbbbaee3e849c12f9a60e3c9a
Barbora Klusackova <barboraklusackova@gmail.com>
2024-02-11 10:45:51 +1300

    Add new styling for initial page

--- app/NoHabitsScreen.tsx
+++ app/NoHabitsScreen.tsx
@@ -8,7 +8,7 @@ export const NoHabitsScreen: React.FC<NoHabitsScreenProps> = ({
   addNewHabit,
 }) => {
   return (
-    <div className="m-3 text-center">
+    <div className="m-3  mt-40 text-center">
       <div className="flex justify-center mb-10 mt-10 font-medium text-lg">
         <p>You are not tracking any habits yet. Go ahead and create one.</p>
       </div>
