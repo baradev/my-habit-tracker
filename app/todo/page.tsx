37d47d12b0fc58bb0e34a6099db1825803e1c1bd
Barbora Klusackova <barboraklusackova@gmail.com>
2024-02-11 14:31:33 +1300

    change to do

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const ToDo = () => {
   return (
-    <div>
-      <ul>
-        <li>Connect with database</li>
-        <li> Initial page when the app is first use</li>
-        <li>when click on homepage - loaded on current month</li>
-      </ul>
+    <div className="flex flex-col items-center m-40">
+      <div>
+        <p>The app is still evolving, here is what is comming soon:</p>
+      </div>
+      <div className="m-10">
+        <ul className="list-disc">
+          <li>Connecting App to Database</li>
+        </ul>
+      </div>
     </div>
   )
 }
